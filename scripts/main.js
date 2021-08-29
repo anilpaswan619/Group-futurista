84b31625d0093ba9b62525b0a97ea68f77797ff8
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2021-08-29 23:14:11 +0530

    Updated

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+  function headerContent(){
+     document.getElementById("header-content").style.display = "none";
+  }
+  function headerShow(){
+     document.getElementById("header-content").style.display = "block";
+  }
